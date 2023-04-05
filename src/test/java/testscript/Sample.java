feb04a18ed4e9a36c8d1e42edec7f719d6af105a
Admin <Admin@DESKTOP-5AB338K>
2023-04-05 13:13:18 +0530

    1 commit

---
+++ src/test/java/testscript/Sample.java
@@ -0,0 +1,14 @@
+package testscript;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	
+	@Test
+	
+	public void delete()
+	{
+		System.out.println("deleted successfully");
+	}
+}
---
+++ src/test/java/testscript/Sample1.java
@@ -0,0 +1,11 @@
+package testscript;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+	@Test
+	public void recover() {
+		System.out.println("recoverd");
+	}
+
+}
